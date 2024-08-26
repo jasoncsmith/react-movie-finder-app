71a40c456b2b7a00662834a67c1a1183bff9a788
Jason C Smith <smithones@gmail.com>
2024-08-25 19:20:39 -0600

    css tweak

--- src/components/MovieFinder/components/NoImage/index.tsx
+++ src/components/MovieFinder/components/NoImage/index.tsx
@@ -1,5 +1,5 @@
 const NoImage = () => (
-  <div className="text-xl w-full h-full flex justify-center items-center">
+  <div className="bg-slate-800 text-xl md:aspect-square text-gray-500 w-full min-h-36 h-full flex justify-center items-center border border-solid border-slate-400">
     <p>No Image Available</p>
   </div>
 )
